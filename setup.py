c05499d777433b9b4eb99a18d66cf531480be9b3
cjmiles <chris.john.miles@gmail.com>
2021-01-03 12:36:52 -0800

    corrected setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 
 from setuptools import setup
 
-setup(name='ml_scratch',
-      version='0.0.1',
-      install_requires=[]
-      )
+setup(
+    name='ml_scratch',
+    version='0.0.1',
+    install_requires=[]
+)
