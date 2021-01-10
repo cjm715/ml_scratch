6aac2af9468d4e76e78eae38390b3a673ea96cb7
cjmiles <chris.john.miles@gmail.com>
2021-01-09 23:36:18 -0800

    cleaned code to use if else statement within fit statement depending on the argument method

--- ml_scratch/logisticRegression.py
+++ ml_scratch/logisticRegression.py
@@ -14,7 +14,27 @@ class logisticRegression:
     def __init__(self):
         pass
 
-    def fit(self, X, y, iterations=100, nu=0.001):
+    def predict(self, X_test):
+        N, M = X_test.shape
+        Xc = add_bias_col(X_test)
+        y = self._hypothesis(Xc).squeeze()
+        return y
+
+    def fit(self, X, y, iterations=None, nu=None, method='gd'):
+        if method == 'gd':
+            if iterations is None:
+                iterations = 100
+            if nu is None:
+                nu = 0.01
+            self._fit_gd(X, y, iterations=iterations, nu=nu)
+        else:  # method == 'newton'
+            if iterations is None:
+                iterations = 10
+            if nu is None:
+                nu = 1
+            self._fit_newton(X, y, iterations=iterations, nu=nu)
+
+    def _fit_gd(self, X, y, iterations=100, nu=0.001):
         N, M = X.shape
         Xc = add_bias_col(X)
         y = y[:, None]
@@ -28,7 +48,7 @@ class logisticRegression:
                 cost = cost.squeeze()
                 print(f'iteration: {i}, cost: {cost}')
 
-    def fit_newtons_method(self, X, y, iterations=100, nu=0.001):
+    def _fit_newton(self, X, y, iterations=100, nu=1):
         N, M = X.shape
         Xc = add_bias_col(X)
         y = y[:, None]
@@ -45,12 +65,6 @@ class logisticRegression:
             cost += (1 - y).T.dot(np.log(1 - self._hypothesis(Xc)))
             cost = cost.squeeze()
             print(f'iteration: {i}, cost: {cost}')
-
-    def predict(self, X_test):
-        N, M = X_test.shape
-        Xc = add_bias_col(X_test)
-        y = self._hypothesis(Xc).squeeze()
-        return y
 
     def _hypothesis(self, Xc):
         return sigmoid(Xc.dot(self.w))
